5106abcff401dfa66604a23833df35ef9ba1dbbd
fathima-nihala <fathimanihala200214@gmail.com>
2025-03-11 01:21:45 +0530

    origin setting

--- event-scheduler-server/server.js
+++ event-scheduler-server/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://event-scheduler-qpnt.vercel.app/',
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
